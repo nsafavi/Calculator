96d3f161d1de243f7c6bd8bdd7555d3cc83f360e
davidap090 <davidpalmer090@outlook.com>
2021-06-11 16:48:33 -0700

    Working Main.cpp

--- Calculator_Project/Main.cpp
+++ Calculator_Project/Main.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-int First_Value;
-int Second_Value;
-int Third_Value;
-int Fourth_Value;
+int Num1;
+int Num2;
+int Num3;
+int Num4;
 int selection;
 int calculation;
 
@@ -15,39 +15,37 @@ void calculate();
 int main() {
 
 	cout << "Enter the first value: ";
-	cin >> First_Value;
+	cin >> Num1;
 
 	cout << "\nEnter the second value: ";
-	cin >> Second_Value; 
+	cin >> Num2; 
 
 	cout << "Choose an operation... \n";
 	cout << "1 - Sum (+) \n";
 	cout << "2 - Subract (-) \n";
 	cout << "3 - Multiply(*)\n";
-	cout << "4 - Divide(/ )\n";
+	cout << "4 - Divide(/)\n";
 	cin >> selection;
 
 	calculate();
-
-	cout << "\n" + calculation;
-
+	cout << calculation;
 	return 0;
 }
 
 void calculate(){
 	if (selection == 1) {
-		First_Value + Second_Value = calculation;
+		calculation = Num1 + Num2;
 	}
 
 	else if (selection == 2) {
-		First_Value - Second_Value = calculation;
+		calculation = Num1 - Num2;
 	}
 
 	else if (selection == 3) {
-		First_Value * Second_Value = calculation;
+		calculation = Num1 * Num2;
 	}
 
 	else if (selection == 4) {
-		First_Value / Second_Value = calculation;
+		calculation = Num1 / Num2;
 	}
 }
